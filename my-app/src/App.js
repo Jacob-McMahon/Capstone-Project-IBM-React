d1e0c27cb92d884db695c0a6c799c6f753b8bce6
jacob-mcmahon <jacobryanmcmahon@gmail.com>
2025-10-05 18:10:19 +0200

    commit before sending it in for assessment

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,6 +9,16 @@ import Navbar from './Components/Navbar/Navbar';
 import Landing_Page from './Components/Landing_Page/LandingPage';
 import Login from './Components/Login/Login';
 import SignUp from './Components/Sign-Up/Sign_Up';
+import InstantConsultation from './Components/InstantConsultationBooking/InstantConsultation';
+import FindDoctorSearch from './Components/FindDoctorSearch/FindDoctorSearch';
+import DoctorCard from './Components/DoctorCard/DoctorCard';
+import AppointmentForm from './Components/AppointmentForm/AppointmentForm';
+import Notification from './Components/Notifications/Notifications';
+
+
+
+import './App.css';
+import ReviewForm from './Components/ReviewForm/ReviewForm';
 
 // Function component for the main App
 function App() {
@@ -27,7 +37,13 @@ function App() {
             <Route path="/" element={<Landing_Page/>}/>
             <Route path="/signup" element={<SignUp/>}/>
             <Route path="/login" element={<Login/>}/>
+            <Route path="/instant-consultation" element={<InstantConsultation />} />
+            <Route path="find-doctor-search" element={<FindDoctorSearch/>} />
+            <Route path="/doctor-card" element={<DoctorCard />} />
+            <Route path="/appointment-form" element={<AppointmentForm />} />
+            <Route path="/review-form" element={<ReviewForm />} />
           </Routes>
+          <Notification />
         </BrowserRouter>
     </div>
   );
